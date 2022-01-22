b3a32058eaeab79e695e710e711d3a43c1341a01
Wliubai <1121324663@qq.com>
2022-01-22 19:38:38 +0800

    first commit

---
+++ 单链表习题1/src/TestDemo.java
@@ -0,0 +1,16 @@
+/**
+ * @program: 20220122
+ * @description
+ * @author: WJJ
+ * @create: 2022-01-22 14:29
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        Node ret = myLinkedList.reverseList();
+        myLinkedList.display(ret);
+
+    }
+
+
+}
