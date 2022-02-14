12a64962da3bcaa8e4fef5f5a5308e8b9bdeab89
Wliubai <1121324663@qq.com>
2022-02-14 15:07:49 +0800

    first commit

---
+++ 20220214/src/TestDemo.java
@@ -0,0 +1,20 @@
+/**
+ * @program: 20220214
+ * @description
+ * @author: WJJ
+ * @create: 2022-02-14 11:26
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList=new MyLinkedList();
+        myLinkedList.addFirst(1);
+        myLinkedList.addFirst(9);
+        myLinkedList.addFirst(3);
+        myLinkedList.addFirst(4);
+        myLinkedList.addFirst(3);
+        myLinkedList.display();
+
+
+
+    }
+}
